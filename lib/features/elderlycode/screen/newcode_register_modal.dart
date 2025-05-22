44aa99bdfeefb4d6a6db954869167028613deaf7
fusiontwo <yr0955@naver.com>
2025-05-22 18:06:28 +0900

    Feat: Add connection with lambda in NewCodeRegisterModal

--- lib/features/elderlycode/screen/newcode_register_modal.dart
+++ lib/features/elderlycode/screen/newcode_register_modal.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:day_in_bloom_fd_v1/features/authentication/service/kakao_auth_service.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class NewCodeRegisterModal extends StatelessWidget {
   const NewCodeRegisterModal({super.key});
@@ -43,9 +47,48 @@ class NewCodeRegisterModal extends StatelessWidget {
       ),
       actions: [
         TextButton(
-          onPressed: () {
-            Navigator.pop(context, codeController.text);
-            // 입력 텍스트 저장 로직 추가
+          onPressed: () async {
+            final inputCode = codeController.text.trim();
+            final userId = await KakaoAuthService.getUserId();
+
+            if (userId == null || inputCode.isEmpty) {
+              print("입력값 누락: userId 또는 code");
+              Navigator.pop(context);
+              return;
+            }
+
+            final body = {
+              "kakao_user_id": userId,
+              "encodedId": inputCode,
+            };
+
+            final url = dotenv.env['NEWCODE_REGISTER_API_GATEWAY_URL'];
+            if (url == null || url.isEmpty) {
+              print("API URL 누락됨");
+              Navigator.pop(context);
+              return;
+            }
+
+            try {
+              final prettyJson = const JsonEncoder.withIndent('  ').convert(body);
+              print("전송할 JSON:\n$prettyJson");
+
+              final response = await http.post(
+                Uri.parse(url),
+                headers: {"Content-Type": "application/json"},
+                body: jsonEncode(body),
+              );
+
+              if (response.statusCode == 200) {
+                print("등록 성공");
+              } else {
+                print("등록 실패: ${response.statusCode}");
+              }
+            } catch (e) {
+              print("API 호출 오류: $e");
+            }
+
+            Navigator.pop(context, inputCode);
           },
           child: const Text(
             "확인",
